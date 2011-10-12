ed8711992a7060887ea10d704d89bf4d40fe264b
Mark Hiner <hiner@wisc.edu>
2011-10-12 14:14:43 -0500

    Added empty Checker interface

---
+++ components/scifio/src/scifio/Checker.java
@@ -0,0 +1,12 @@
+package scifio;
+
+/**
+ * Interface for all SciFIO Checker objects.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="">Trac</a>,
+ * <a href="">Gitweb</a></dd></dl>
+ */
+public interface Checker {
+
+}
