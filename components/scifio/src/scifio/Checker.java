b5bd76278234fd33b1262dd456f8345fb9082b68
Mark Hiner <hiner@wisc.edu>
2011-10-13 15:06:11 -0500

    Updated Checker interface
    The Checker interface should be largely complete now.

--- components/scifio/src/scifio/Checker.java
+++ components/scifio/src/scifio/Checker.java
@@ -1,5 +1,9 @@
 package scifio;
 
+import java.io.IOException;
+
+import loci.common.RandomAccessInputStream;
+
 /**
  * Interface for all SciFIO Checker objects.
  *
@@ -7,6 +11,24 @@ package scifio;
  * <dd><a href="">Trac</a>,
  * <a href="">Gitweb</a></dd></dl>
  */
-public interface Checker {
+public interface Checker<M extends Metadata> extends MetadataHandler<M>, FormatHandler {
 
+	// -- Checker API methods --
+	
+	/**
+	 * Checks if the given file is a valid instance of this file format.
+	 *
+	 * @param open If true, and the file extension is insufficient to determine
+	 *   the file type, the file may be opened for further analysis, or other
+	 *   relatively expensive file system operations (such as file existence
+	 *   tests and directory listings) may be performed.  If false, file system
+	 *   access is not allowed.
+	 */
+	boolean supportsFormat(String name, boolean open);
+
+	/** Checks if the given block is a valid header for this file format. */
+	boolean supportsFormat(byte[] block);
+
+	/** Checks if the given stream is a valid stream for this file format. */
+	boolean supportsFormat(RandomAccessInputStream stream) throws IOException;
 }
