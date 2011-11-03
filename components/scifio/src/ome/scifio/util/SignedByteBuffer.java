1dc1689c25b5b5292c068bbcbabdb41f03191ef5
Mark Hiner <hiner@wisc.edu>
2011-11-03 11:40:34 -0500

    Added BufferedImage utility classes
    The loci.formats SignedByteBuffer, SignedShortBuffer, UnsignedIntBuffer
    were migrated to SCIFIO, along with a BufferedImage tools method -
    extracting logic previously encapsulated int he loci.formats.gui
    package.

---
+++ components/scifio/src/ome/scifio/util/SignedByteBuffer.java
@@ -0,0 +1,62 @@
+package ome.scifio.util;
+
+
+import java.awt.image.DataBuffer;
+
+/**
+ * DataBuffer that stores signed bytes.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/gui/SignedByteBuffer.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/gui/SignedByteBuffer.java;hb=HEAD">Gitweb</a></dd></dl>
+ *
+ * @author Melissa Linkert melissa at glencoesoftware.com
+ */
+public class SignedByteBuffer extends DataBuffer {
+
+  private byte[][] bankData;
+
+  /** Construct a new buffer of signed bytes using the given byte array.  */
+  public SignedByteBuffer(byte[] dataArray, int size) {
+    super(DataBuffer.TYPE_BYTE, size);
+    bankData = new byte[1][];
+    bankData[0] = dataArray;
+  }
+
+  /** Construct a new buffer of signed bytes using the given 2D byte array. */
+  public SignedByteBuffer(byte[][] dataArray, int size) {
+    super(DataBuffer.TYPE_BYTE, size);
+    bankData = dataArray;
+  }
+
+  /* @see java.awt.image.DataBuffer.getData() */
+  public byte[] getData() {
+    return bankData[0];
+  }
+
+  /* @see java.awt.image.DataBuffer#getData(int) */
+  public byte[] getData(int bank) {
+    return bankData[bank];
+  }
+
+  /* @see java.awt.image.DataBuffer#getElem(int) */
+  public int getElem(int i) {
+    return getElem(0, i);
+  }
+
+  /* @see java.awt.image.DataBuffer#getElem(int, int) */
+  public int getElem(int bank, int i) {
+    return (int) bankData[bank][i + getOffsets()[bank]];
+  }
+
+  /* @see java.awt.image.DataBuffer#setElem(int, int) */
+  public void setElem(int i, int val) {
+    setElem(0, i, val);
+  }
+
+  /* @see java.awt.image.DataBuffer#setElem(int, int, int) */
+  public void setElem(int bank, int i, int val) {
+    bankData[bank][i + getOffsets()[bank]] = (byte) val;
+  }
+
+}
---
+++ components/scifio/src/ome/scifio/util/UnsignedIntBuffer.java
@@ -0,0 +1,103 @@
+package ome.scifio.util;
+
+
+import java.awt.image.DataBuffer;
+
+/**
+ * DataBuffer that stores unsigned ints.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/gui/UnsignedIntBuffer.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/gui/UnsignedIntBuffer.java;hb=HEAD">Gitweb</a></dd></dl>
+ *
+ * @author Melissa Linkert melissa at glencoesoftware.com
+ */
+public class UnsignedIntBuffer extends DataBuffer {
+
+  private int[][] bankData;
+
+  /** Construct a new buffer of unsigned ints using the given int array.  */
+  public UnsignedIntBuffer(int[] dataArray, int size) {
+    super(DataBuffer.TYPE_INT, size);
+    bankData = new int[1][];
+    bankData[0] = dataArray;
+  }
+
+  /** Construct a new buffer of unsigned ints using the given 2D int array. */
+  public UnsignedIntBuffer(int[][] dataArray, int size) {
+    super(DataBuffer.TYPE_INT, size);
+    bankData = dataArray;
+  }
+
+  /* @see java.awt.image.DataBuffer.getData() */
+  public int[] getData() {
+    return bankData[0];
+  }
+
+  /* @see java.awt.image.DataBuffer#getData(int) */
+  public int[] getData(int bank) {
+    return bankData[bank];
+  }
+
+  /* @see java.awt.image.DataBuffer#getElem(int) */
+  public int getElem(int i) {
+    return getElem(0, i);
+  }
+
+  /* @see java.awt.image.DataBuffer#getElem(int, int) */
+  public int getElem(int bank, int i) {
+    int value = bankData[bank][i + getOffsets()[bank]];
+    return (int) (value & 0xffffffffL);
+  }
+
+  /* @see java.awt.image.DataBuffer#getElemFloat(int) */
+  public float getElemFloat(int i) {
+    return getElemFloat(0, i);
+  }
+
+  /* @see java.awt.image.DataBuffer#getElemFloat(int, int) */
+  public float getElemFloat(int bank, int i) {
+    return (float) (getElem(bank, i) & 0xffffffffL);
+  }
+
+  /* @see java.awt.image.DataBuffer#getElemDouble(int) */
+  public double getElemDouble(int i) {
+    return getElemDouble(0, i);
+  }
+
+  /* @see java.awt.image.DataBuffer#getElemDouble(int, int) */
+  public double getElemDouble(int bank, int i) {
+    return (double) (getElem(bank, i) & 0xffffffffL);
+  }
+
+  /* @see java.awt.image.DataBuffer#setElem(int, int) */
+  public void setElem(int i, int val) {
+    setElem(0, i, val);
+  }
+
+  /* @see java.awt.image.DataBuffer#setElem(int, int, int) */
+  public void setElem(int bank, int i, int val) {
+    bankData[bank][i + getOffsets()[bank]] = val;
+  }
+
+  /* @see java.awt.image.DataBuffer#setElemFloat(int, float) */
+  public void setElemFloat(int i, float val) {
+    setElemFloat(0, i, val);
+  }
+
+  /* @see java.awt.image.DataBuffer#setElemFloat(int, int, float) */
+  public void setElemFloat(int bank, int i, float val) {
+    bankData[bank][i + getOffsets()[bank]] = (int) val;
+  }
+
+  /* @see java.awt.image.DataBuffer#setElemDouble(int, double) */
+  public void setElemDouble(int i, double val) {
+    setElemDouble(0, i, val);
+  }
+
+  /* @see java.awt.image.DataBuffer#setElemDouble(int, int, double) */
+  public void setElemDouble(int bank, int i, double val) {
+    bankData[bank][i + getOffsets()[bank]] = (int) val;
+  }
+
+}
