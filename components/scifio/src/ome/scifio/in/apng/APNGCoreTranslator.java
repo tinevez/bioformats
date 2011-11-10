7ff4728acec841b4d2f42e45ecc412bdadf4aadc
Mark Hiner <hiner@wisc.edu>
2011-11-10 13:47:47 -0600

    Added comments/javadoc to APNGCoreTranslator
    Added a header block with class summary, and a clear start of the
    Translator API Methods

--- components/scifio/src/ome/scifio/in/apng/APNGCoreTranslator.java
+++ components/scifio/src/ome/scifio/in/apng/APNGCoreTranslator.java
@@ -3,8 +3,15 @@ package ome.scifio.in.apng;
 import ome.scifio.AbstractTranslator;
 import ome.scifio.Metadata;
 
+/**
+ * File format SCIFIO Translator for Animated Portable Network Graphics
+ * (APNG) images to the Core SCIFIO image type.
+ *
+ */
 public class APNGCoreTranslator extends AbstractTranslator<APNGMetadata>{
 
+	// -- Translator API Methods --
+	
 	@Override
 	public Metadata translate(APNGMetadata metaIn) {
 		// TODO Auto-generated method stub
