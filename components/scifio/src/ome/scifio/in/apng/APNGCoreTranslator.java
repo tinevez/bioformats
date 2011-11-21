868ee216c34e0386045a6e42510607261f85b285
Mark Hiner <hiner@wisc.edu>
2011-11-21 14:51:51 -0600

    WIP: APNGCoreTranslator implementation
    This is a rough implementation of the APNG : Core Translator component.
    The two formats are very similar, so most values are simply copied
    between the source and destination Metadata objects.

--- components/scifio/src/ome/scifio/in/apng/APNGCoreTranslator.java
+++ components/scifio/src/ome/scifio/in/apng/APNGCoreTranslator.java
@@ -1,21 +1,46 @@
 package ome.scifio.in.apng;
 
 import ome.scifio.AbstractTranslator;
-import ome.scifio.Metadata;
+import ome.scifio.CoreImageMetadata;
+import ome.scifio.CoreMetadata;
 
 /**
  * File format SCIFIO Translator for Animated Portable Network Graphics
  * (APNG) images to the Core SCIFIO image type.
  *
  */
-public class APNGCoreTranslator extends AbstractTranslator<APNGMetadata> {
+public class APNGCoreTranslator extends AbstractTranslator<APNGMetadata, CoreMetadata> {
 
   // -- Translator API Methods --
 
   @Override
-  public Metadata translate(APNGMetadata metaIn) {
-    // TODO Auto-generated method stub
-    return null;
+  public void translate(APNGMetadata source, CoreMetadata dest) {
+    CoreImageMetadata coreMeta = new CoreImageMetadata();
+    CoreMetadata meta = new CoreMetadata();
+    meta.add(coreMeta);
+    
+    // TODO use setters.. clear?
+    coreMeta.sizeX = source.sizeX;
+    coreMeta.sizeY = source.sizeY;
+    coreMeta.sizeZ = source.sizeZ;
+    coreMeta.sizeC = source.sizeC;
+    coreMeta.sizeT = source.sizeT;
+    coreMeta.thumbSizeX = source.thumbSizeX;
+    coreMeta.thumbSizeY = source.thumbSizeY;
+    coreMeta.pixelType = source.pixelType;
+    coreMeta.pixelType = source.bitsPerPixel;
+    coreMeta.cLengths = source.cLengths;
+    coreMeta.cTypes = source.cTypes;
+    coreMeta.dimensionOrder = source.dimensionOrder;
+    coreMeta.orderCertain = source.orderCertain;
+    coreMeta.rgb = source.rgb;
+    coreMeta.littleEndian = source.littleEndian;
+    coreMeta.interleaved = source.interleaved;
+    coreMeta.indexed = source.indexed;
+    coreMeta.falseColor = true;
+    coreMeta.metadataComplete = source.metadataComplete;
+    coreMeta.imageMetadata = source.imageMetadata;
+    coreMeta.thumbnail = source.thumbnail;
   }
 
   // -- MetadataHandler API Methods --
