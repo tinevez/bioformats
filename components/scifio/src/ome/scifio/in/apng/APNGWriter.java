c77e785f3141e6af4f2ab89016a54f4882445805
Mark Hiner <hiner@wisc.edu>
2011-12-02 14:26:00 -0600

    First pass implementation of APNGWriter
    This is a non-functional (but compile-safe) implementation of the
    APNGWriter.  Pending the core Writer functionality changes, the
    APNGWriter will also need specific changes to take advantage of the
    additional metadata that is parsed by ome.scifio.in.apng.APNGParser
    (relative to the original loci.formats.out.APNGWriter).

--- components/scifio/src/ome/scifio/in/apng/APNGWriter.java
+++ components/scifio/src/ome/scifio/in/apng/APNGWriter.java
@@ -1,12 +1,15 @@
 package ome.scifio.in.apng;
 
-import java.awt.image.ColorModel;
+import java.awt.image.IndexColorModel;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
-import loci.formats.codec.CodecOptions;
+import java.util.zip.CRC32;
+import java.util.zip.DeflaterOutputStream;
 
 import ome.scifio.AbstractWriter;
 import ome.scifio.FormatException;
+import ome.scifio.common.DataTools;
+import ome.scifio.util.FormatTools;
 
 public class APNGWriter extends AbstractWriter<APNGMetadata> {
 
@@ -32,190 +35,32 @@ public class APNGWriter extends AbstractWriter<APNGMetadata> {
   // -- Writer API Methods --
   
   /**
-   * @see loci.formats.IFormatWriter#saveBytes(int, byte[], int, int, int, int)
+   * @see ome.scifio.Writer#saveBytes(int, byte[], int, int, int, int)
    */
-  public void saveBytes(int no, byte[] buf, int x, int y, int w, int h)
+  public void saveBytes(int iNo, int no, byte[] buf, int x, int y, int w, int h)
     throws FormatException, IOException
   {
-    /* TODO
-    checkParams(no, buf, x, y, w, h);
-    if (!isFullPlane(x, y, w, h)) {
+    checkParams(iNo, no, buf, x, y, w, h);
+    if (!isFullPlane(iNo, x, y, w, h)) {
       throw new FormatException(
         "APNGWriter does not yet support saving image tiles.");
     }
-    MetadataRetrieve meta = getMetadataRetrieve();
 
-    int width = meta.getPixelsSizeX(series).getValue().intValue();
-    int height = meta.getPixelsSizeY(series).getValue().intValue();
+    int width = metadata.getSizeX(iNo);
+    int height = metadata.getSizeY(iNo);
 
-    if (!initialized[series][no]) {
+    if (!initialized[iNo][no]) {
       writeFCTL(width, height);
       if (numFrames == 0) writePLTE();
-      initialized[series][no] = true;
+      initialized[iNo][no] = true;
     }
 
-    writePixels(numFrames == 0 ? "IDAT" : "fdAT", buf, x, y, w, h);
+    writePixels(iNo, numFrames == 0 ? "IDAT" : "fdAT", buf, x, y, w, h);
     numFrames++;
-    */
   }
 
-  @Override
-  public void saveBytes(int no, byte[] buf) throws FormatException, IOException
-  {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void savePlane(int no, Object plane)
-    throws FormatException, IOException
-  {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void savePlane(int no, Object plane, int x, int y, int w, int h)
-    throws FormatException, IOException
-  {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void setSeries(int series) throws FormatException {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public int getSeries() {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-  @Override
-  public void setValidBitsPerPixel(int bits) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public boolean canDoStacks() {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public void setColorModel(ColorModel cm) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public ColorModel getColorModel() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public void setFramesPerSecond(int rate) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public int getFramesPerSecond() {
-    // TODO Auto-generated method stub
-    return 0;
-  }
-
-  @Override
-  public String[] getCompressionTypes() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public int[] getPixelTypes() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public int[] getPixelTypes(String codec) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public boolean isSupportedType(int type) {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public void setCompression(String compress) throws FormatException {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void setCodecOptions(CodecOptions options) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public String getCompression() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public void changeOutputFile(String id) throws FormatException, IOException {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void setWriteSequentially(boolean sequential) {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void saveBytes(byte[] bytes, boolean last)
-    throws FormatException, IOException
-  {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void saveBytes(byte[] bytes, int series, boolean lastInSeries,
-    boolean last) throws FormatException, IOException
-  {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void savePlane(Object plane, boolean last)
-    throws FormatException, IOException
-  {
-    // TODO Auto-generated method stub
-    
-  }
-
-  @Override
-  public void savePlane(Object plane, int series, boolean lastInSeries,
-    boolean last) throws FormatException, IOException
-  {
-    // TODO Auto-generated method stub
-    
-  }
-
+  // -- MetadataHandler methods --
+  
   @Override
   public Class<APNGMetadata> getMetadataType() {
     // TODO Auto-generated method stub
@@ -223,4 +68,212 @@ public class APNGWriter extends AbstractWriter<APNGMetadata> {
   }
 
   // -- APNGWriter Methods --
+  
+  /* @see ome.scifio.Writer#close() */
+  public void close() throws IOException {
+    if (out != null) {
+      writeFooter();
+    }
+    super.close();
+    numFrames = 0;
+    numFramesPointer = 0;
+    nextSequenceNumber = 0;
+    littleEndian = false;
+  }
+  
+  // TODO where to call this?  Should it be in the API?
+  @Override
+  public void initialize(int iNo) throws FormatException, IOException {
+    super.initialize(iNo);
+    if (out.length() == 0) {
+      int width = metadata.getSizeX(iNo);
+      int height = metadata.getSizeY(iNo);
+      int bytesPerPixel =
+        FormatTools.getBytesPerPixel(metadata.getPixelType(iNo));
+      int nChannels = metadata.getEffectiveSizeC(iNo);
+      boolean indexed =
+        getColorModel() != null && (getColorModel() instanceof IndexColorModel);
+      littleEndian = metadata.isLittleEndian(iNo);
+
+      // write 8-byte PNG signature
+      out.write(PNG_SIG);
+
+      // write IHDR chunk
+
+      out.writeInt(13);
+      byte[] b = new byte[17];
+      b[0] = 'I';
+      b[1] = 'H';
+      b[2] = 'D';
+      b[3] = 'R';
+
+      DataTools.unpackBytes(width, b, 4, 4, false);
+      DataTools.unpackBytes(height, b, 8, 4, false);
+
+      b[12] = (byte) (bytesPerPixel * 8);
+      if (indexed) b[13] = (byte) 3;
+      else if (nChannels == 1) b[13] = (byte) 0;
+      else if (nChannels == 2) b[13] = (byte) 4;
+      else if (nChannels == 3) b[13] = (byte) 2;
+      else if (nChannels == 4) b[13] = (byte) 6;
+      b[14] = (byte) 0;
+      b[15] = (byte) 0;
+      b[16] = (byte) 0;
+
+      out.write(b);
+      out.writeInt(crc(b));
+
+      // write acTL chunk
+
+      out.writeInt(8);
+      out.writeBytes("acTL");
+      numFramesPointer = out.getFilePointer();
+      out.writeInt(0);
+      out.writeInt(0);
+      out.writeInt(0); // save a place for the CRC
+    }
+  }
+  
+  private int crc(byte[] buf) {
+    return crc(buf, 0, buf.length);
+  }
+
+  private int crc(byte[] buf, int off, int len) {
+    CRC32 crc = new CRC32();
+    crc.update(buf, off, len);
+    return (int) crc.getValue();
+  }
+
+  private void writeFCTL(int width, int height) throws IOException {
+    out.writeInt(26);
+    byte[] b = new byte[30];
+    b[0] = 'f';
+    b[1] = 'c';
+    b[2] = 'T';
+    b[3] = 'L';
+
+    DataTools.unpackBytes(nextSequenceNumber++, b, 4, 4, false);
+    DataTools.unpackBytes(width, b, 8, 4, false);
+    DataTools.unpackBytes(height, b, 12, 4, false);
+    DataTools.unpackBytes(0, b, 16, 4, false);
+    DataTools.unpackBytes(0, b, 20, 4, false);
+    DataTools.unpackBytes(1, b, 24, 2, false);
+    DataTools.unpackBytes(fps, b, 26, 2, false);
+    b[28] = (byte) 1;
+    b[29] = (byte) 0;
+
+    out.write(b);
+    out.writeInt(crc(b));
+  }
+
+  private void writePLTE() throws IOException {
+    if (!(getColorModel() instanceof IndexColorModel)) return;
+
+    IndexColorModel model = (IndexColorModel) getColorModel();
+    byte[][] lut = new byte[3][256];
+    model.getReds(lut[0]);
+    model.getGreens(lut[1]);
+    model.getBlues(lut[2]);
+
+    out.writeInt(768);
+    byte[] b = new byte[772];
+    b[0] = 'P';
+    b[1] = 'L';
+    b[2] = 'T';
+    b[3] = 'E';
+
+    for (int i=0; i<lut[0].length; i++) {
+      for (int j=0; j<lut.length; j++) {
+        b[i*lut.length + j + 4] = lut[j][i];
+      }
+    }
+
+    out.write(b);
+    out.writeInt(crc(b));
+  }
+
+  private void writePixels(int iNo, String chunk, byte[] stream, int x, int y,
+    int width, int height) throws FormatException, IOException
+  {
+    int sizeC = metadata.getEffectiveSizeC(iNo);
+    String type = FormatTools.getPixelTypeString(metadata.getPixelType(iNo));
+    int pixelType = FormatTools.pixelTypeFromString(type);
+    boolean signed = FormatTools.isSigned(pixelType);
+
+    if (!isFullPlane(iNo, x, y, width, height)) {
+      throw new FormatException("APNGWriter does not support writing tiles.");
+    }
+
+    ByteArrayOutputStream s = new ByteArrayOutputStream();
+    s.write(chunk.getBytes());
+    if (chunk.equals("fdAT")) {
+      s.write(DataTools.intToBytes(nextSequenceNumber++, false));
+    }
+    DeflaterOutputStream deflater = new DeflaterOutputStream(s);
+    int planeSize = stream.length / sizeC;
+    int rowLen = stream.length / height;
+    int bytesPerPixel = stream.length / (width * height * sizeC);
+    byte[] rowBuf = new byte[rowLen];
+    for (int i=0; i<height; i++) {
+      deflater.write(0);
+      if (interleaved) {
+        if (littleEndian) {
+          for (int col=0; col<width*sizeC; col++) {
+            int offset = (i * sizeC * width + col) * bytesPerPixel;
+            int pixel = DataTools.bytesToInt(stream, offset,
+              bytesPerPixel, littleEndian);
+            DataTools.unpackBytes(pixel, rowBuf, col * bytesPerPixel,
+              bytesPerPixel, false);
+          }
+        }
+        else System.arraycopy(stream, i * rowLen, rowBuf, 0, rowLen);
+      }
+      else {
+        int max = (int) Math.pow(2, bytesPerPixel * 8 - 1);
+        for (int col=0; col<width; col++) {
+          for (int c=0; c<sizeC; c++) {
+            int offset = c * planeSize + (i * width + col) * bytesPerPixel;
+            int pixel = DataTools.bytesToInt(stream, offset, bytesPerPixel,
+              littleEndian);
+            if (signed) {
+              if (pixel < max) pixel += max;
+              else pixel -= max;
+            }
+            int output = (col * sizeC + c) * bytesPerPixel;
+            DataTools.unpackBytes(pixel, rowBuf, output, bytesPerPixel, false);
+          }
+        }
+      }
+      deflater.write(rowBuf);
+    }
+    deflater.finish();
+    byte[] b = s.toByteArray();
+
+    // write chunk length
+    out.writeInt(b.length - 4);
+    out.write(b);
+
+    // write checksum
+    out.writeInt(crc(b));
+  }
+
+  private void writeFooter() throws IOException {
+    // write IEND chunk
+    out.writeInt(0);
+    out.writeBytes("IEND");
+    out.writeInt(crc("IEND".getBytes()));
+
+    // update frame count
+    out.seek(numFramesPointer);
+    out.writeInt(numFrames);
+    out.skipBytes(4);
+    byte[] b = new byte[12];
+    b[0] = 'a';
+    b[1] = 'c';
+    b[2] = 'T';
+    b[3] = 'L';
+    DataTools.unpackBytes(numFrames, b, 4, 4, false);
+    DataTools.unpackBytes(0, b, 8, 4, false);
+    out.writeInt(crc(b));
+  }
 }
